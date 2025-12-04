f68266dfa7ea52a72a417487a02c6c1da746d93b
Bala <balasudahn17@gmail.com>
2025-12-04 16:56:39 +0530

    udated

---
+++ reactprojects/VITE/myvite/src/NavBar.jsx
@@ -0,0 +1,22 @@
+ const NavBar = ()=>{
+
+    return (
+      <>
+          
+      <div>
+this is react
+      </div>
+      <div>
+react is Navbar
+      </div>
+      </>
+  
+    )
+        
+ 
+
+
+} 
+
+
+export default NavBar
