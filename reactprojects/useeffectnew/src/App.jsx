1f9133d6c88398cc80e9552454eaeb32db3ac2da
Bala <balasudahn17@gmail.com>
2025-12-26 19:20:12 +0530

    Timeer on

--- reactprojects/useeffectnew/src/App.jsx
+++ reactprojects/useeffectnew/src/App.jsx
@@ -1,6 +1,7 @@
 
 // import { useEffect, useState } from 'react'
 
+import NewTimer from "./components/NewTimer"
 import Timer from "./components/Timer"
 
 
@@ -60,7 +61,8 @@ const App = () => {
 
 
 
-<Timer/>
+{/* <Timer/> */}
+<NewTimer/>
     
    </>
   )
