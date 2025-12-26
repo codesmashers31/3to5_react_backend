64282dcf94ae139b99e8d7067352a23a2466c591
Bala <balasudahn17@gmail.com>
2025-12-26 16:23:10 +0530

    updated

---
+++ reactprojects/useeffectnew/src/App.jsx
@@ -0,0 +1,71 @@
+
+// import { useEffect, useState } from 'react'
+
+import Timer from "./components/Timer"
+
+
+const App = () => {
+
+  // const [data,setData] = useState([])
+
+
+
+  // console.log('I am the main componenets')
+
+  
+ 
+
+
+  // useEffect(()=>{
+
+  //   console.log('I am useEffect');
+    
+
+  //     const fetchapi = async()=>{
+
+  //    const takethelink = await fetch("https://dummyjson.com/products");
+  //    const changedata = await takethelink.json();
+
+  //    setData(changedata.products)
+
+
+
+  // }
+
+  //     fetchapi()
+
+ 
+
+
+  // },[])
+
+
+
+
+
+
+
+
+  return (
+   <>
+   <div>UseEffect Process</div>
+  
+
+
+  {/* <div>
+    {data.map((e)=>(
+      <h1 key={e.id}>{e.title}</h1>
+    ))}
+  </div> */}
+
+
+
+<Timer/>
+    
+   </>
+  )
+}
+
+export default App
+
+
---
+++ reactprojects/useeffectnew/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    console.log('i Am Contact');
+    
+  return (
+    <div>
+      Contact
+    </div>
+  )
+}
+
+export default Contact
