ae9edd1acd4ff3c93603e2cdb51481b37ef6ba1c
Bala <balasudahn17@gmail.com>
2026-01-05 17:15:23 +0530

    updated

---
+++ reactprojects/usecontext_process/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
+
+const Navbar = () => {
+  const {getdata} = useContext(ThemeContext)
+  return (
+    <div className={getdata?"bg-blue-500 text-white p-10 h-100":"bg-gray-600 text-white p-10 h-100"}>
+    <h1>{getdata?"Yes its light":"Yes its dark"}</h1>
+    </div>
+  )
+}
+
+export default Navbar
